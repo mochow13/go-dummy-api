7ce566caf082b60c5a1dcb52e295fc40b7873d34
mochow13 <md.mottakin.chowdhury@gmail.com>
2019-05-17 02:21:14 +0600

    finish up

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"log"
 	"math/rand"
 	"net/http"
 	"strconv"
@@ -28,6 +30,7 @@ type Author struct {
 var books []Book
 
 func getBooks(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("he")
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(books)
 }
@@ -53,12 +56,17 @@ func createBook(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(book)
 }
 
-func updateBook(w http.ResponseWriter, r *http.Request) {
-
-}
-
 func deleteBook(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range books {
+		if item.ID == params["id"] {
+			// gets rid of the book :|
+			books = append(books[:index], books[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(books)
 }
 
 func buildDB() {
@@ -88,8 +96,7 @@ func main() {
 	router.HandleFunc("/api/books", getBooks).Methods("GET")
 	router.HandleFunc("/api/books/{id}", getOneBook).Methods("GET")
 	router.HandleFunc("/api/books", createBook).Methods("POST")
-	router.HandleFunc("/api/books/{id}", updateBook).Methods("GET")
-	router.HandleFunc("/api/books/{id}", deleteBook).Methods("GET")
+	router.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
 	// run server
-	(http.ListenAndServe(":8000", router))
+	log.Fatal(http.ListenAndServe(":8000", router))
 }
